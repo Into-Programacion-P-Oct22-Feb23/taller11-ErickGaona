6d0ebac3dc1a2ada924f0b9737200f5b2b5d1786
Erick Gaona <erickgaona625@gmail.com>
2023-01-07 15:37:49 -0500

    Taller 11 grupal funciones y procedimientos

--- ejercicios/Problemas/src/problemas/Problema6.java
+++ ejercicios/Problemas/src/problemas/Problema6.java
@@ -10,22 +10,55 @@ package problemas;
  * @author reroes
  */
 public class Problema6 {
+
+    static int media;
+
     public static void main(String[] args) {
         int[] informacion = {1, 2, 3, 10, 20, 30, 100, 200, 300};
-        
+
         double mediaAritmetica = mediaAritmetica(informacion);
-        double estandar = desviacionEstandar(informacion, 
-                mediaAritmetica);
-        
+        double estandar = desviacionEstandar(informacion, mediaAritmetica);
+
         System.out.printf("El valor de media aritmetica es %.2f y el valor de "
-                + "La desviación estándar %.2f", mediaAritmetica,estandar);
+                + "La desviación estándar %.2f \n", mediaAritmetica, estandar);
+
     }
-    
-    public static double mediaAritmetica(int [] a) {
-        
+
+    public static double mediaAritmetica(int[] a) {
+        int suma = 0;
+
+        int i;
+        for (i = 0; i < a.length; i++) {
+            suma = suma + a[i];
+        }
+        media = suma / i;
+        return media;
+
     }
-    public static double desviacionEstandar(int [] a, double b) {
-        
+
+    public static double desviacionEstandar(int[] a, double b) {
+        double desviacion;
+        double suma = 0;
+        double suma2=0;
+        double desviacion2=0;
+        for (int i = 0; i < a.length; i++) {
+
+            desviacion = Math.pow(a[i] - media,2);
+            suma = suma + desviacion;
+            suma2 = suma / 9;
+
+            desviacion2 = Math.sqrt(suma2);
+//            System.out.println(a[i] + " - " + media + "| " + desviacion);
+//            System.out.println("--------------------------");
+//            System.out.println("aqui:"+suma);
+//            System.out.println("-----------------------------");
+//            System.out.println("here:"+suma2);
+
+            //System.out.println("xd"+suma);
+            
+        }
+        return desviacion2;
+
     }
-       
+
 }
--- ejercicios/Problemas/src/problemas/Problema7.java
+++ ejercicios/Problemas/src/problemas/Problema7.java
@@ -5,10 +5,52 @@
  */
 package problemas;
 
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author reroes
  */
 public class Problema7 {
-    
+
+    static String resul;
+
+    public static void main(String[] args) {
+        String cadenas = arreglo();
+        System.out.println("Las ciudades de EC son: " + cadenas);
+
+        procedimiento();
+
+    }
+
+    public static String arreglo() {
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Ingrese Numero de elementos: ");
+        int num = entrada.nextInt();
+        resul = " ";
+        String[] arreglo = new String[num];
+        entrada.nextLine();
+
+        for (int i = 0; i < arreglo.length; i++) {
+            System.out.println("Ingrese una Ciudad: ");
+            String ciudad = entrada.nextLine();
+            arreglo[i] = ciudad;
+            resul = String.format("%s%s ", resul, arreglo[i]);
+
+        }
+        return resul;
+
+    }
+
+    public static void procedimiento() {
+        //System.out.println(resul);
+        String resultado = "";
+        if (resul.length() >= 9 && resul.length() <= 5) {
+            resultado = String.format("%s%s", resultado, resul);
+            System.out.println("Los eleomentos con 4 o 5 caracteres son:"
+                    + " " + resultado);
+        }
+    }
+
 }
